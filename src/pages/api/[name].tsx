e7434446b934c53adcc32be6709414c541d7c7e0
Alex-Programer <alex.lichangnan@gmail.com>
2023-04-25 18:33:19 +0800

    chore: 编写ing

--- src/pages/api/[name].tsx
+++ src/pages/api/[name].tsx
@@ -1,18 +1,71 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import satori from "satori";
+
+const loadFont = async (req: NextApiRequest) => {
+  return fetch(
+    `${req.headers["x-forwarded-proto"]}://${req.headers.host}/Roboto-Regular.ttf`
+  ).then((res) => res.arrayBuffer());
+};
+
+const getMeta = async (name: string) => {
+  return fetch(
+    `https://indexer.crossbell.io/v1/handles/${name}/character`
+  ).then((res) => res.json());
+};
+
+const getViewCount = async (characterId: string) => {
+  return fetch(
+    `https://indexer.crossbell.io/v1/stat/characters/${characterId}`
+  ).then((res) => res.json());
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { name } = req.query;
+  const { name, theme, layout } = req.query as {
+    name: string;
+    theme: string;
+    layout: string;
+  };
 
-  const font = await fetch(
-    `${req.headers["x-forwarded-proto"]}://${req.headers.host}/Roboto-Regular.ttf`
-  ).then((res) => res.arrayBuffer());
+  const metaData = await getMeta(name);
+  const characterId = metaData.characterId;
 
-  const svg = await satori(<div>123</div>, {
+  const cards = [
+    {
+      id: "articles",
+      title: "文章总数",
+      getData: async () => {},
+    },
+    {
+      id: "articles",
+      title: "评论总数",
+      getData: async () => {},
+    },
+    {
+      id: "articles",
+      title: "关注者总数",
+      getData: async () => {},
+    },
+    {
+      id: "viewNoteCount",
+      title: "浏览总数",
+      getData: async () => {
+        const { viewNoteCount } = await getViewCount(characterId);
+        return viewNoteCount;
+      },
+    },
+    {
+      id: "articles",
+      title: "站点运行时间",
+      getData: async () => {},
+    },
+  ];
+
+  // 浏览总数
+  const font = await loadFont(req);
+  const svg = await satori(<div style={{ display: "flex" }}>666</div>, {
     width: 100,
     height: 100,
     fonts: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,83 @@
-export default function Home() {
-  return <img src="http://localhost:3000/api/hello" alt="" />;
-}
+import React, { useState } from "react";
+
+const card: { light: React.CSSProperties; dark: React.CSSProperties } = {
+  light: {
+    backgroundColor: "#fff",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+    padding: "20px",
+  },
+  dark: {
+    backgroundColor: "#222",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px rgba(255, 255, 255, 0.1)",
+    padding: "20px",
+  },
+};
+
+const cardHeader: React.CSSProperties = {
+  backgroundColor: "#00bcd4",
+  borderRadius: "10px 10px",
+  color: "#fff",
+  padding: "10px 20px",
+};
+
+const cardTitle: React.CSSProperties = {
+  margin: "0",
+};
+
+const cardBody: React.CSSProperties = {
+  color: "#333",
+  padding: "20px 0",
+};
+
+const Card = () => {
+  const [name, setName] = useState("Alex-Programer");
+
+  return (
+    <div style={{ maxWidth: "60%", margin: "0 auto" }}>
+      <h1>xlog card</h1>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onInput={(e) => {
+            setName((e.target as HTMLInputElement).value);
+          }}
+        />
+        <button>识别</button>
+      </div>
+      <h2>主题跟随系统</h2>
+      <picture>
+        <source
+          media="(prefers-color-scheme: light)"
+          srcSet={`http://localhost:3000/api/${name}?theme=light`}
+        />
+        <source
+          media="(prefers-color-scheme: dark)"
+          srcSet={`http://localhost:3000/api/${name}?theme=dark`}
+        />
+        <img src={`http://localhost:3000/api/${name}?theme=light`} alt="" />
+      </picture>
+      <div style={card.light}>
+        <div style={cardHeader}>
+          <h2 style={cardTitle}>Light</h2>
+        </div>
+        <div style={cardBody}>
+          <img src={`http://localhost:3000/api/${name}?theme=light`} alt="" />
+        </div>
+      </div>
+      <div style={{ height: 100 }}></div>
+      <div style={card.dark}>
+        <div style={cardHeader}>
+          <h2 style={cardTitle}>Dark</h2>
+        </div>
+        <div style={cardBody}>
+          <img src={`http://localhost:3000/api/${name}?theme=dark`} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
